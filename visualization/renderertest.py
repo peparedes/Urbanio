ed7cb4ffb7c7b3789c7078677cde02c714d6577e
Javier Rosa <javirosa1912@gmail.com>
2015-10-10 03:16:11 -0700

    Render test works. Use file test2.txt as an example and is opened by default.

--- visualization/renderertest.py
+++ visualization/renderertest.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import socket
 import struct
+import time
 # import tkinter
 
 MBED_IP = '192.168.1.11'
-MBED_PORT = '54322'
-MBED_CLOCK_PORT = '49000'
+MBED_PORT = 54322
 MBED_CLOCK_IP = '192.168.1.11'
+MBED_CLOCK_PORT = 49000
 
 
 def set_clock(ip, port, value):
@@ -25,35 +26,45 @@ def send_cmd(ip, port, cmd):
 
 
 def mk_cmd(light, r, g, b, ts):
-    return 'L,{0},{1},{2},{3},{4},'.format(light, r, b, g, ts)
+    return bytes(u'L,{0},{1},{2},{3},{4},'.format(light, r, b, g, ts), 'ascii')
 
 
 # Probably need to chunk packets
 def send_program(ip, port, lines):
     set_clock(ip, port, 0)
-    for line in lines:
+    while len(lines):
+        line = b''
+        for y in range(0, 20):
+            while len(lines) and y < 20:
+                line += bytes(lines.pop(0), 'ascii')
         send_cmd(ip, port, line)
+        #TODO wait as long as the commands take
+        time.sleep(1)
 
 
 def blink_all(ip, port, dur=1000, times=3):
     prog = []
     latency = 10
-    for x in (1, 2, 3, 4):
-        cmd = mk_cmd(x, 0, 0, 0, latency)
+    for l in (4, 2, 1, 3):
+        cmd = mk_cmd(l, 0, 0, 0, latency)
         prog.append(cmd)
-    for x in range(1, times):
-        for x in (1, 2, 3, 4):
-            cmd = mk_cmd(x, 255, 255, 255, dur*x + latency)
+    for t in range(1, times):
+        for l in (4, 2, 1, 3):
+            cmd = mk_cmd(l, 255, 255, 255, dur*t + latency)
             prog.append(cmd)
-        for x in (1, 2, 3, 4):
-            cmd = mk_cmd(x, 0, 0, 0, dur*x + dur/2 + latency)
+        for l in (4, 2, 1, 3):
+            cmd = mk_cmd(l, 0, 0, 0, dur*t + dur/2 + latency)
             prog.append(cmd)
     send_program(ip, port, prog)
 
 
 def main():
-    for x in range(0, 10):
-        blink_all('192.168.1.11', 54322, 1000, 6)
+    f = open('test2.txt', 'r')
+    lines = f.readlines()
+    send_program(MBED_IP, MBED_PORT, lines)
+    f.close()
+    # for x in range(0, 1):
+    #    blink_all('192.168.1.11', 54322, 2000, 6)
 
 
 if __name__ == "__main__":
